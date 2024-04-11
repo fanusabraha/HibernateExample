17a4d2f837c35420a696c8b157ec20edb9c58431
fanus abraha <fanus.abraha@yahoo.com>
2024-04-11 07:46:50 +0200

    Creating Webpage for users to acces the their data

--- src/main/java/com/Hibernate/WebApp/Service/CustomerService.java
+++ src/main/java/com/Hibernate/WebApp/Service/CustomerService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -13,6 +14,10 @@ public class CustomerService {
     CustomerRepository customerRepository;
     public List<Customer> findAll(){
         return customerRepository.findAll();
+    }
+    public Customer findById(Long id){
+        Optional <Customer> findById =  customerRepository.findById(id);
+        return findById.orElse(new Customer());
     }
 
 }
--- src/main/java/com/Hibernate/WebApp/web/CustomerController.java
+++ src/main/java/com/Hibernate/WebApp/web/CustomerController.java
@@ -6,17 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("")
 public class CustomerController {
     @Autowired
     CustomerService customerService;
-    @GetMapping("")
+    @GetMapping("/customers")
     public String findAllCustomers (ModelMap map){
         List<Customer> allCustomers = customerService.findAll();
+        map.put("customers",allCustomers);
     return "Bank-Customers";}
+    @GetMapping("/customers/{id}")
+    public String findById (ModelMap map, @PathVariable Long id){
+        Customer oneCustomer = customerService.findById(id);
+        map.put("customers",oneCustomer);
+        return "Bank-Customers";}
 }
